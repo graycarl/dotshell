5a23bb1fd0f2639f59b10b00c046c0f65984a2b5
Hongbo He <hongbo.he@veeva.com>
2019-06-21 17:46:19 +0800

    Improve sample

--- samples.sh
+++ samples.sh
@@ -1,9 +1,1 @@
-function hbkit() {
-    if `hash hbkit &> /dev/null`; then
-        command hbkit $@
-    else
-        cmd="$HOME/.virtualenvs/N/bin/hbkit"
-        echo Using $cmd
-        $cmd $@
-    fi
-}
+alias hbkit='PATH=$PATH:$HOME/.virtualenvs/N/bin/ command hbkit'
