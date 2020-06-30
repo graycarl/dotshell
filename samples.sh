0041d8f52763d4762b287060e72c53ba39d02a7f
Hongbo He <hhbcarl@gmail.com>
2020-06-30 12:11:05 +0800

    Move omzsh to local

--- samples.sh
+++ samples.sh
@@ -1,1 +1,21 @@
 alias hbkit='PATH=$PATH:$HOME/.virtualenvs/N/bin/ command hbkit'
+
+# virtualenvwrapper
+# pip3 install --user virtualenvwrapper
+# pip install --user virtualenvwrapper
+PATH=$PATH:$HOME/Library/Python/3.7/bin/
+PATH=$PATH:$HOME/Library/Python/2.7/bin/
+
+if [[ $SHELL == "/bin/zsh" ]]; then
+    ZSH_THEME="hhb-ys"
+
+    plugins=(git virtualenv virtualenvwrapper)
+    if [[ $TERM_PROGRAM = "vscode" ]]; then
+        ZSH_THEME="embeded"
+        plugins=(git virtualenv)
+    fi
+    if [[ -n $VIM ]]; then
+        ZSH_THEME="embeded"
+        plugins=(git virtualenv)
+    fi
+fi
--- zshrc.sh
+++ zshrc.sh
@@ -10,11 +10,11 @@ source $DOTSHELL/base.sh
 plugins=(git)
 ZSH_THEME="robbyrussell"
 DISABLE_AUTO_UPDATE="true"
-test -e $DOTSHELL/omzrc.sh && source $DOTSHELL/omzrc.sh
-source $ZSH/oh-my-zsh.sh
 
-# I do want to share history
+# I don't want to share history
 unsetopt share_history
 
 source $DOTSHELL/lib.sh
 test -e $HOME/.shlocal && source $HOME/.shlocal
+
+source $ZSH/oh-my-zsh.sh
