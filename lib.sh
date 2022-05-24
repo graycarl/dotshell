4f700ec2c5f3e2361ac753549ecd3ae49d46555b
Hongbo He <hhbcarl@gmail.com>
2022-05-24 17:44:49 +0800

    Add tmux-init

--- lib.sh
+++ lib.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Common functions
 
 # Tools
@@ -66,12 +67,44 @@ function brew-fix-venv() {
     echo "Recreate virtualenv using $p"
     virtualenv -p $p $venv
 }
+
 # https://github.com/pypa/virtualenv/issues/2023#issuecomment-748636276
 function fix-py2-venv-for-m1() {
     pushd $1/bin
     mkdir bk; cp python bk; mv -f bk/python .;rmdir bk
     codesign -s - --preserve-metadata=identifier,entitlements,flags,runtime -f python
     popd
+}
+
+# Put xxx.session in CONFIG_DIR
+# eg.
+# cat xxx.session
+# editor ~/Sources/ChinaSFA
+# debug ~/Sources/ChinaSFA
+TMUX_INIT_CONFIG=$HOME/.config/tmux-init
+function tmux-init() {
+    set -e
+    local name
+    local wd
+    for file in $TMUX_INIT_CONFIG/*.session; do
+        session_name=$(basename $file .session)
+        while read name wd; do
+            if [[ -z ${session_created+x} ]]; then
+                tmux new-session -s $session_name -n $name -c "${wd/#\~/$HOME}" -d
+                session_created=1
+            else
+                tmux new-window -n $name -c "${wd/#\~/$HOME}"
+            fi
+        done < $file
+        unset session_created
+    done
+}
+function tmux-clean() {
+    set -e
+    for file in $TMUX_INIT_CONFIG/*.session; do
+        session_name=$(basename $file .session)
+        tmux kill-session -t $session_name
+    done
 }
 
 # Markp: Convert markdown to presentation in html / pdf
