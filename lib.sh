1b4c95012d6296e80074353c610a64b9bf6fa1d2
Hongbo He <hongbo.he@veeva.com>
2023-02-01 17:21:24 +0800

    Remove unused lib and add gui-prompt

--- lib.sh
+++ lib.sh
@@ -11,45 +11,6 @@ function notify() {
     osascript -e "display notification \"$1\" with title \"$title\""
 }
 
-# rbenv init
-function init-rbenv() {
-    eval "$(rbenv init -)"
-}
-
-# Go Workspace
-function init-go() {
-    if which -s go > /dev/null; then
-        export GOROOT=`go env GOROOT`
-        export GOPATH=$HOME/gospace
-        export PATH=$PATH:$GOPATH/bin
-    else
-        echo "Golang not found."
-    fi
-}
-
-# nvm
-function init-nvm() {
-    export NVM_DIR="$HOME/.nvm"
-    source "$(brew --prefix nvm)/nvm.sh"
-}
-
-# List software installed
-function list-installed-apps() {
-    CMDS=('mas list' 'brew list' 'brew cask list' 'ls -d /Applications/Setapp/*.app')
-    echo "Generate on $(hostname) at $(date '+%Y-%m-%d %H:%M:%S')"
-    echo
-    for cmd in $CMDS; do
-        echo "# $cmd"
-        eval $cmd
-        echo
-    done
-}
-function backup-installed-list() {
-    fn="$HOME/iCloud/Backups/InstalledApps/$(hostname).$(date +%Y-%m-%d).installedapps.txt"
-    list-installed-apps > $fn
-    echo "Installed apps list saved to $fn"
-}
-
 # Python
 # See: https://stackoverflow.com/a/25947333
 function brew-fix-venv() {
@@ -108,6 +69,11 @@ function tmux-clean() {
     done
 }
 
-# Markp: Convert markdown to presentation in html / pdf
-# See: https://hub.docker.com/r/marpteam/marp-cli/
-alias markp='docker run --rm --init -v $PWD:/home/marp/app/ -e LANG=$LANG marpteam/marp-cli'
+# Show GUI prompt for input
+function gui-prompt() {
+  osascript <<EOT
+    tell app "System Events"
+      text returned of (display dialog "$1" default answer "$2" buttons {"OK"} default button 1 with title "$(basename $0)")
+    end tell
+EOT
+}
