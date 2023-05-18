5373f465bf5c3f7cac2fbe25318b74603aaf2b78
Hongbo He <hhbcarl@gmail.com>
2023-05-18 21:32:50 +0800

    Updated on Hongbo-Mac-Studio.local

--- tools/git-sync.sh
+++ tools/git-sync.sh
@@ -28,52 +28,58 @@ function is_conflict() {
     fi
 }
 
-debug "Script start: $1"
-if [[ -n $1 ]]; then
-    cd $1
-fi
+function main() {
 
-if is_conflict; then
-    warn "The repository is in conflict state, abort!"
-    exit
-fi
+    debug "Script start: $1"
+    if [[ -n $1 ]]; then
+        cd $1
+    fi
 
-debug "Commit local"
-if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
-    git add .
-    git commit -qm "Updated on $(hostname)"
-else
-    log "No local new commit"
-fi
-
-debug "Fetch remote"
-git fetch -q origin
-
-debug "Merge remote"
-if git merge-base --is-ancestor origin/master HEAD; then
-    log "Local file is update to date"
-elif git merge-base --is-ancestor HEAD origin/master; then
-    log "Process a fast-forward."
-    git merge --ff-only > /dev/null
-else
-    log "Process a merge"
-    if git merge > /dev/null; then
-        log "Merge succeeded."
-    else
-        warn "Conflict found"
+    if is_conflict; then
+        warn "The repository is in conflict state, abort!"
         exit
     fi
-fi
-debug
 
-debug "Update remote"
-if ! git merge-base --is-ancestor origin/master HEAD; then
-    warn "Something wrong?"
-    exit 1
-fi
-if [[ "$(git rev-parse origin/master)" == "$(git rev-parse HEAD)" ]]; then
-    log "Remote file is update to date"
-else
-    log "Push to remote"
-    git push
-fi
+    debug "Commit local"
+    if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
+        git add .
+        git commit -qm "Updated on $(hostname)"
+    else
+        log "No local new commit"
+    fi
+
+    debug "Fetch remote"
+    git fetch -q origin
+
+    debug "Merge remote"
+    if git merge-base --is-ancestor origin/master HEAD; then
+        log "Local file is update to date"
+    elif git merge-base --is-ancestor HEAD origin/master; then
+        log "Process a fast-forward."
+        git merge --ff-only > /dev/null
+    else
+        log "Process a merge"
+        if git merge > /dev/null; then
+            log "Merge succeeded."
+        else
+            warn "Conflict found"
+            exit
+        fi
+    fi
+    debug
+
+    debug "Update remote"
+    if ! git merge-base --is-ancestor origin/master HEAD; then
+        warn "Something wrong?"
+        exit 1
+    fi
+    if [[ "$(git rev-parse origin/master)" == "$(git rev-parse HEAD)" ]]; then
+        log "Remote file is update to date"
+    else
+        log "Push to remote"
+        git push
+    fi
+
+}
+
+main
