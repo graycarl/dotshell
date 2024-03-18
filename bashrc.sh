f4fab081fdb3e1151ab0b4f9ad9763385a9414dd
Hongbo He <hongbo.he@veeva.com>
2024-03-18 14:21:15 +0800

    Move .shlocal to .shell/local/shrc

--- bashrc.sh
+++ bashrc.sh
@@ -3,4 +3,5 @@ export DOTSHELL=$HOME/.shell
 source $DOTSHELL/base.sh
 source $DOTSHELL/lib.sh
 
-test -e $HOME/.shlocal && source $HOME/.shlocal
+test -e $DOTSHELL/local/shrc && source $DOTSHELL/local/shrc
+test -e $HOME/.shlocal && echo 'Please move .shlocal to .shell/local/shrc'
--- zshrc.sh
+++ zshrc.sh
@@ -12,7 +12,8 @@ ZSH_THEME="robbyrussell"
 DISABLE_AUTO_UPDATE="true"
 
 source $DOTSHELL/lib.sh
-test -e $HOME/.shlocal && source $HOME/.shlocal
+test -e $DOTSHELL/local/shrc && source $DOTSHELL/local/shrc
+test -e $HOME/.shlocal && echo 'Please move .shlocal to .shell/local/shrc'
 
 source $ZSH/oh-my-zsh.sh
 
