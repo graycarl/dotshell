85c251f2eb6f01a8d0159d48279a20f4ac3d9e65
Hongbo He <hhbcarl@gmail.com>
2026-01-22 01:05:05 +0800

    refactor: rename tern-end-notify to agent-end-notify and trigger on agent_end

--- apps/pi/agent/extensions/tern-end-notify.ts
+++
@@ -1,9 +0,0 @@
-import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-
-// See: <https://github.com/badlogic/pi-mono/tree/main/packages/coding-agent#custom-tools>
-export default function (pi: ExtensionAPI) {
-  // Send notification on macOS when tern end.
-  pi.on("turn_end", async (event, ctx) => {
-    await pi.exec("osascript", ["-e", `display notification "Turn ended" with title "Pi Mono"`]);
-  });
-}
